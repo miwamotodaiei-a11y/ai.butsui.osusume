0532a89431f1808f54b14e2f0d7f6ce618147bee
miwamotodaiei-a11y <miwamotodaiei@gmail.com>
2026-01-14 23:27:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -42,12 +42,10 @@ if api_key:
     【最初の第一声】
     「こんにちは、今日はどうしましたか？」といった風に始めてください。
     """
-    
     model = genai.GenerativeModel(
     model_name="gemini-1.5-flash",
     system_instruction=my_instruction
 　　)
-
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
