eebaa5ae733404a1ef60e4f6cc298d8eabc9bd4d
miwamotodaiei-a11y <miwamotodaiei@gmail.com>
2026-01-14 23:19:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ if api_key:
     """
     
     model = genai.GenerativeModel(
-        model_name="gemini-1.5-flash-latest",
+        model_name="models/gemini-1.5-flash",
         system_instruction=my_instruction
     )
 
